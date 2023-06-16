e02764406db5eda10b3036abced1c33dbda92f7b
sixaliyevas <anar.sixaliyev@kapitalbank.az>
2023-06-16 17:52:19 +0400

    write more code for local game mode

--- code/mian1.py
+++ code/mian1.py
@@ -1,3 +1,9 @@
+import argparse
+import itertools
+import os
+import pickle
+import sys
+import time
 from timeit import default_timer as timer
 
 import numpy as np
@@ -31,11 +37,62 @@ def local_playing():
         print("AI Played in postion ({},{})".format(pos_x, pos_y))
         print("====================")
 
-    #   game progress
+    while True:
+        if game_end(game_board, target) == agent_player:
+            print("GAME_OVER: COMPUTER WINS")
+            break
+        if game_end(game_board, target) == opp_player:
+            print("GAME_OVER: HUMAN WINS")
+            break
+
+        if tie_game(game_board):
+            print("GAME_OVER: TIE")
+            break
+
+        print("Human Plays...")
+        pos_x, pos_y = opp_play()
+        while (game_board[pos_x, pos_y] != 0):
+            print("Select an empty cell please:")
+            pos_x, pos_y = opp_play()
+        game_board[pos_x, pos_y] = opp_player
+        print(game_board)
+        print("Human Played")
+        print("====================") 
+
+        if game_end(game_board, target) == opp_player:
+            print("GAME_OVER: HUMAN WINS")
+            break
+
+        if tie_game(game_board):
+            print("GAME_OVER: TIE") 
+            break
+        print("AI Plays...")
+        pos_x, pos_y = best_next_move(
+            game_board, target, agent_player, opp_player)
+        game_board[pos_x, pos_y] = agent_player
+        print(game_board)
+        print("AI Played in postion ({},{})".format(pos_x, pos_y))
+        print("====================")
 
 
 def main():
     local_playing()
+    # board_size = 6
+    # target = 4
+    # dpth = 3
+    # mem = {}
+    # bf = 'board_evals/b_{}_{}.pkl'.format(board_size, target)
+    # if os.path.isfile(bf):
+    #     with open(bf, 'rb') as fp:
+    #         mem = pickle.load(fp)
+    #         print("Learned Boards:")
+    #         print("Count of learned boards:{}".format(len(mem)))
+
+    # if (len(mem) > 300000):
+    #     mem = dict(itertools.islice(mem.items(), 300000))
+    # with open(bf, 'wb') as fp:
+    #     pickle.dump(mem, fp)
+    #     print('dictionary saved successfully to file')
 
 
 if __name__ == "__main__":
