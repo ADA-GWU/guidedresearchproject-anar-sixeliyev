a5f6a5ad2d29c51779cdea27ca920c2d61b9564e
sixaliyevas <anar.sixaliyev@kapitalbank.az>
2023-07-13 11:38:06 -0400

    Added board file to check board conditions

---
+++ code/board.py
@@ -0,0 +1,70 @@
+
+import numpy as np
+
+
+def tie_game(board):
+    return np.count_nonzero(board) == board.size
+
+
+def check_rows(board, target):
+    for x in range(board.shape[0]):
+        for y in range(board.shape[0]-target+1):
+            if np.all(board[x, y:y+target] == 1):
+                return 1
+            elif np.all(board[x, y:y+target] == -1):
+                return -1
+    return 0
+
+
+def check_columns(board, target):
+    for x in range(board.shape[0]-target+1):
+        for y in range(board.shape[0]):
+            if np.all(board[x:x+target, y] == 1):
+                return 1
+            elif np.all(board[x:x+target, y] == -1):
+                return -1
+    return 0
+
+
+def check_diagonals(board, target):
+    for x in range(board.shape[0]-target+1):
+        for y in range(board.shape[0]-target+1):
+            if np.all(np.diag(board[x:x+target, y:y+target]) == 1):
+                return 1
+            elif np.all(np.diag(board[x:x+target, y:y+target]) == -1):
+                return -1
+
+            if np.all(np.diag(np.fliplr(board[x:x+target, y:y+target])) == 1):
+                return 1
+            elif np.all(np.diag(np.fliplr(board[x:x+target, y:y+target])) == -1):
+                return -1
+    return 0
+
+
+def game_end(board, target):
+    r = check_rows(board, target)
+    if r != 0:
+        return r
+    c = check_columns(board, target)
+    if c != 0:
+        return c
+    d = check_diagonals(board, target)
+    if d != 0:
+        return d
+
+    return 0
+
+
+def terminated(board, target):
+    return game_end(board, target) != 0 or tie_game(board)
+
+
+def get_successors(board, player):
+    arr_successors = []
+    for i in range(board.shape[0]):
+        for j in range(board.shape[0]):
+            if board[i][j] == 0:
+                temp_board = board.copy()
+                temp_board[i][j] = player
+                arr_successors.append(temp_board)
+    return arr_successors
