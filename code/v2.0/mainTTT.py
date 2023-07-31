11ebb09fa123d9482c272135cfaf8a396bd3aeb9
sixaliyevas <anar.sixaliyev@kapitalbank.az>
2023-07-31 10:14:34 -0400

    Fixed AI vs AI

--- code/v2.0/mainTTT.py
+++ code/v2.0/mainTTT.py
@@ -13,64 +13,157 @@ from heuristic import *
 from minimax import *
 
 
-def ai_vs_ai(board_size: int = 3, target_size: int = 3, depth=3, memory={}):
-    """
-  This function is used to play tic-tac-toe between same AI vs AI.
-  Goal of self playing is for memorizing board evaluations.
-  """
-    # s_all = timer()
-    game_board = np.zeros((board_size, board_size), dtype=np.int32)
-    agent1 = 1
-    agent2 = -1
+# def ai_vs_ai(board_size: int = 3, target_size: int = 3, depth=3, memory={}):
+#     """
+#   This function is used to play tic-tac-toe between same AI vs AI.
+#   Goal of self playing is for memorizing board evaluations.
+#   """
+#     # s_all = timer()
+#     game_board = np.zeros((board_size, board_size), dtype=np.int32)
+#     agent1 = 1
+#     agent2 = -1
 
-    while True:
-        s1 = timer()
-        print("AI_1 Plays...")
-        pos_x, pos_y = best_next_move(game_board, target_size, agent1, agent2,
-                                      depth, memory)
-        game_board[pos_x, pos_y] = agent1
-        #         print_board(game_board)
-        print(game_board)
-        e1 = timer()
-        print("AI_1 Played in postion ({},{})".format(pos_x, pos_y))
-        print("Execution time {}".format(e1 - s1))
+#     while True:
+#         s1 = timer()
+#         print("AI_1 Plays...")
+#         pos_x, pos_y = best_next_move(game_board, target_size, agent1, agent2,
+#                                       depth, memory)
+#         game_board[pos_x, pos_y] = agent1
+#         #         print_board(game_board)
+#         print(game_board)
+#         e1 = timer()
+#         print("AI_1 Played in postion ({},{})".format(pos_x, pos_y))
+#         print("Execution time {}".format(e1 - s1))
+#         print("====================")
+
+#         is_game_end = game_end(game_board, target_size)
+#         if is_game_end == 1:
+#             print("AI_1 won the game")
+#             break
+#         if is_game_end == 2:
+#             print("AI_2 won the game")
+#             break
+
+#         if tie_game(game_board):
+#             print("GAME_OVER: TIE")
+#             break
+
+#         print("AI_2 Plays...")
+#         s2 = timer()
+#         pos_x, pos_y = best_next_move(game_board, target_size, agent2, agent1,
+#                                       depth, memory)
+#         game_board[pos_x, pos_y] = agent2
+#         #         print_board(game_board)
+#         print(game_board)
+#         print("AI_2 Played in postion ({},{})".format(pos_x, pos_y))
+#         e2 = timer()
+#         print("Execution time {}".format(e2 - s2))
+#         print("====================")
+
+#         is_game_end = game_end(game_board, target_size)
+#         if is_game_end == 1:
+#             print("AI_1 won the game")
+#             break
+#         if is_game_end == 2:
+#             print("AI_2 won the game")
+#             break
+
+#         if tie_game(game_board):
+#             print("GAME_OVER: TIE")
+#             break
+
+
+def ai_vs_ai(num_games=10):
+    board_size = 3
+    target = 3
+    agent_player = 1
+    memory = {}
+    total_moves = 0
+    total_time = 0
+    ai1_wins = 0
+    ai2_wins = 0
+    ties = 0
+
+    for game_num in range(1, num_games + 1):
+        print(f"Game {game_num}:")
+        game_board = np.zeros(
+            (board_size, board_size, board_size, board_size), dtype=np.int32)
+        opp_player = -agent_player
+        NEXT_BOARD_X, NEXT_BOARD_Y = -1, -1
+
+        start_time = time.time()
+
+        while True:
+            if check_large_board_winner(game_board, agent_player):
+                print("GAME_OVER: AI 1 WINS")
+                ai1_wins += 1
+                break
+            if check_large_board_winner(game_board, opp_player):
+                print("GAME_OVER: AI 2 WINS")
+                ai2_wins += 1
+                break
+
+            if tie_game_big(game_board):
+                print("GAME_OVER: TIE")
+                ties += 1
+                break
+
+            if NEXT_BOARD_X == -1 or NEXT_BOARD_Y == -1 or check_small_board_winner(game_board[NEXT_BOARD_X, NEXT_BOARD_Y], agent_player) or check_small_board_winner(game_board[NEXT_BOARD_X, NEXT_BOARD_Y], opp_player):
+                print('<====BOARD {} {} IS WON, AI 1 SELECTS A NEW BOARD====>\n'.format(
+                    NEXT_BOARD_X, NEXT_BOARD_Y))
+                pos_X, pos_Y, pos_x, pos_y = best_next_move_ult(
+                    game_board, target, agent_player, opp_player, 1, memory)
+            else:
+                print('=== AI 1 RUNS best_next_move_small_large')
+                pos_X, pos_Y, pos_x, pos_y = best_next_move_small_large(
+                    game_board, pos_x, pos_y, target, agent_player, opp_player, 1, memory)
+
+            game_board[pos_X, pos_Y][pos_x, pos_y] = agent_player
+            NEXT_BOARD_X = pos_x
+            NEXT_BOARD_Y = pos_y
+
+            print_ultimate_board(game_board)
+            total_moves += 1
+
+            if check_large_board_winner(game_board, agent_player):
+                print("GAME_OVER: AI 1 WINS")
+                ai1_wins += 1
+                break
+
+            if tie_game_big(game_board):
+                print("GAME_OVER: TIE")
+                ties += 1
+                break
+
+            if NEXT_BOARD_X == -1 or NEXT_BOARD_Y == -1 or check_small_board_winner(game_board[NEXT_BOARD_X, NEXT_BOARD_Y], agent_player) or check_small_board_winner(game_board[NEXT_BOARD_X, NEXT_BOARD_Y], opp_player):
+                print('<====BOARD {} {} IS WON, AI 2 SELECTS A NEW BOARD====>\n'.format(
+                    NEXT_BOARD_X, NEXT_BOARD_Y))
+                pos_X, pos_Y, pos_x, pos_y = best_next_move_ult(
+                    game_board, target, opp_player, agent_player, 1, memory)
+            else:
+                print('=== AI 2 RUNS best_next_move_small_large')
+                pos_X, pos_Y, pos_x, pos_y = best_next_move_small_large(
+                    game_board, pos_x, pos_y, target, opp_player, agent_player, 1, memory)
+
+            game_board[pos_X, pos_Y][pos_x, pos_y] = opp_player
+            NEXT_BOARD_X = pos_x
+            NEXT_BOARD_Y = pos_y
+
+            print_ultimate_board(game_board)
+            total_moves += 1
+
+        end_time = time.time()
+        game_time = end_time - start_time
+        total_time += game_time
+        print(
+            f"Game {game_num} time: {game_time:.2f} seconds, total moves: {total_moves}")
         print("====================")
 
-        is_game_end = game_end(game_board, target_size)
-        if is_game_end == 1:
-            print("AI_1 won the game")
-            break
-        if is_game_end == 2:
-            print("AI_2 won the game")
-            break
-
-        if tie_game(game_board):
-            print("GAME_OVER: TIE")
-            break
-
-        print("AI_2 Plays...")
-        s2 = timer()
-        pos_x, pos_y = best_next_move(game_board, target_size, agent2, agent1,
-                                      depth, memory)
-        game_board[pos_x, pos_y] = agent2
-        #         print_board(game_board)
-        print(game_board)
-        print("AI_2 Played in postion ({},{})".format(pos_x, pos_y))
-        e2 = timer()
-        print("Execution time {}".format(e2 - s2))
-        print("====================")
-
-        is_game_end = game_end(game_board, target_size)
-        if is_game_end == 1:
-            print("AI_1 won the game")
-            break
-        if is_game_end == 2:
-            print("AI_2 won the game")
-            break
-
-        if tie_game(game_board):
-            print("GAME_OVER: TIE")
-            break
+    print("AI 1 Wins:", ai1_wins)
+    print("AI 2 Wins:", ai2_wins)
+    print("Ties:", ties)
+    print("Average Game Time:", total_time / num_games)
+    print("Average Moves per Game:", total_moves / num_games)
 
 
 def opp_play():
@@ -193,12 +286,21 @@ def main():
                         action=argparse.BooleanOptionalAction,
                         default=False)
 
+    parser.add_argument("-ai_vs_ai",
+                        "--ai_vs_ai",
+                        dest="ai_vs_ai",
+                        help="If you want to play AI vs AI mode",
+                        action=argparse.BooleanOptionalAction,
+                        default=False)
     args = parser.parse_args()
 
     boolean_local = args.local_playing
+    ai_vs_ai_mode = args.ai_vs_ai
 
     if boolean_local:
         local_playing()
+    if ai_vs_ai_mode:
+        ai_vs_ai()
 
 
 if __name__ == "__main__":
